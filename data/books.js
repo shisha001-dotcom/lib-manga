56e80fd6fd7b873f48e24afc1d5746acc241eacb
shisha001-dotcom <shisha001@dulieu16.com>
2026-05-20 11:09:19 +0700

    Update books.js

--- data/books.js
+++ data/books.js
@@ -3,25 +3,26 @@ const books = [];
 /* Danh sách file truyện */
 const bookFiles = [
   "conan",
-  "onepiece",
-  "naruto"
+  "onepiece"
 ];
 
 /* Load từng file */
 Promise.all(
   bookFiles.map(name =>
-    fetch(`js/data/${name}.js`)
+    fetch(`data/${name}.js`)   // FIX
       .then(res => res.text())
       .then(code => eval(code))
   )
 ).then(() => {
-  /* Tính dữ liệu */
+
   books.forEach(book => {
     book.owned = book.volumes.filter(v => v.owned).length;
     book.total = book.volumes.length;
     book.progress = Math.round((book.owned / book.total) * 100);
+
+    /* lấy bìa đại diện từ tập 1 */
+    book.cover = book.volumes[0]?.cover || "";
   });
 
-  /* báo app.js rằng đã load xong */
   window.dispatchEvent(new Event("booksLoaded"));
 });
