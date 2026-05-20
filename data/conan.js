598dcb6b226d54ae611e0fa1506ea1f206350560
shisha001-dotcom <shisha001@dulieu16.com>
2026-05-20 23:40:25 +0700

    Update conan.js

--- data/conan.js
+++ data/conan.js
@@ -7,6 +7,7 @@ books.push({
   volumes: [
     { number: 1, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/i/m/image_63429.jpg" },
     { number: 2, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/t/h/tham-tu-lung-danh-conan---tap-2---tb-2023.jpg" },
-    { number: 3, owned: false, cover: "https://cdn1.fahasa.com/media/catalog/product/t/h/tham-tu-lung-danh-conan---tap-3---tb-2023.jpg" }
+    { number: 3, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/t/h/tham-tu-lung-danh-conan---tap-3---tb-2023.jpg" }
+    { number: 4, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/t/h/tham-tu-lung-danh-conan---tap-4---tb-2023.jpg" },
   ]
 });
