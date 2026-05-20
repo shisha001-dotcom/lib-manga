8e481551ca7b596bb942595fcfa94747a4d05984
shisha001-dotcom <shisha001@dulieu16.com>
2026-05-20 11:13:39 +0700

    Update cover image URL for volume 1

--- data/onepiece.js
+++ data/onepiece.js
@@ -5,7 +5,7 @@ books.push({
   shelf: "reading",
 
   volumes: [
-    { number: 1, owned: true, cover: "..." },
+    { number: 1, owned: true, cover: "https://cdn1.fahasa.com/media/flashmagazine/images/page_images/one_piece_1/2022_10_17_10_55_54_1-390x510.jpg" },
     { number: 2, owned: false, cover: "..." }
   ]
 });
