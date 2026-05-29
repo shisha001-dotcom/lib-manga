a49acc7dc826ec82608524cd8ad89f6c5c093e3a
shisha001-dotcom <shisha001@dulieu16.com>
2026-05-29 22:43:30 +0700

    Create mashle.js

---
+++ data/mashle.js
@@ -0,0 +1,27 @@
+books.push({
+  id: "mashle",
+  title: "Mashle",
+  author: "Hajime Kōmoto",
+  shelf: "next",
+
+  volumes: [
+    { number: 1, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/m/a/mashle_tap-1_bia.jpg" },
+    { number: 2, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/m/a/mashle_tap-2_bia.jpg" },
+    { number: 3, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/m/a/mashle_tap-3_bia.jpg" },
+    { number: 4, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/m/a/mashle_tap-4_bia.jpg" },
+    { number: 5, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/m/a/mashle_tap-5_bia.jpg" },
+    { number: 6, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/m/a/mashle_tap-6_bia.jpg" },
+    { number: 7, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/m/a/mashle_tap-7_bia.jpg" },
+    { number: 8, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/m/a/mashle_tap-8_bia.jpg" },
+    { number: 9, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/m/a/mashle_tap-9_bia.jpg" },
+    { number: 10, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/m/a/mashle_tap-10_bia.jpg" },
+    { number: 11, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/m/a/mashle_tap-11_bia.jpg" },
+    { number: 12, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/m/a/mashle_tap-12_bia.jpg" },
+    { number: 13, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/m/a/mashle_tap-13_bia.jpg" },
+    { number: 14, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/m/a/mashle_tap-14_bia.jpg" },
+    { number: 15, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/m/a/mashle_tap-15_bia.jpg" },
+    { number: 16, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/m/a/mashle_tap-16_bia.jpg" },
+    { number: 17, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/m/a/mashle_tap-17_bia.jpg" },
+    { number: 18, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/m/a/mashle_tap-18_bia.jpg" }
+  ]
+});
