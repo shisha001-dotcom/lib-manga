183a9fdee14575859ed19d00a32a7efcc6b163f6
shisha001-dotcom <shisha001@dulieu16.com>
2026-05-29 22:54:06 +0700

    Update onepiece.js

--- data/onepiece.js
+++ data/onepiece.js
@@ -2,7 +2,7 @@ books.push({
   id: "onepiece",
   title: "One Piece",
   author: "Eiichiro Oda",
-  shelf: "reading",
+  shelf: "finished",
 
   volumes: [
     { number: 1, owned: true, cover: "https://cdn1.fahasa.com/media/flashmagazine/images/page_images/one_piece_1/2022_10_17_10_55_54_1-390x510.jpg" },
