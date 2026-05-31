15b625539dcaaaa11ed04b7845feb73e5c80fe4c
shisha001-dotcom <shisha001@dulieu16.com>
2026-05-31 13:24:08 +0700

    Create gachiakuta.js

---
+++ data/gachiakuta.js
@@ -0,0 +1,21 @@
+books.push({
+  id: "gachiakuta",
+  title: "Gachiakuta",
+  author: "Kei Urana, Hideyoshi Andou",
+  shelf: "next",
+
+  volumes: [
+    { number: 1, owned: true, cover: "https://cdn1.fahasa.com/media/catalog/product/n/x/nxbtre_full_12002024_020039_1.jpg" },
+    { number: 2, owned: true, productUrl: "https://cdn1.fahasa.com/media/catalog/product/n/x/nxbtre_full_29042024_040448.jpg" },
+    { number: 3, owned: true, productUrl: "https://cdn1.fahasa.com/media/catalog/product/n/x/nxbtre_full_12082024_110811.jpg" },
+    { number: 4, owned: true, productUrl: "https://cdn1.fahasa.com/media/catalog/product/n/x/nxbtre_full_25102024_091057_1.jpg" },
+    { number: 5, owned: true, productUrl: "https://cdn1.fahasa.com/media/catalog/product/n/x/nxbtre_full_07292024_032938.jpg" },
+    { number: 6, owned: true, productUrl: "https://cdn1.fahasa.com/media/catalog/product/n/x/nxbtre_full_24402024_024047.jpg" },
+    { number: 7, owned: true, productUrl: "https://cdn1.fahasa.com/media/catalog/product/n/x/nxbtre_full_07052024_110534.jpg" },
+    { number: 8, owned: true, productUrl: "https://cdn1.fahasa.com/media/catalog/product/n/x/nxbtre_full_03342024_083413_1.jpg" },
+    { number: 9, owned: true, productUrl: "https://cdn1.fahasa.com/media/catalog/product/n/x/nxbtre_full_17462024_084651.jpg" },
+    { number: 10, owned: true, productUrl: "https://cdn1.fahasa.com/media/catalog/product/n/x/nxbtre_full_31022024_110259.jpg" },
+    { number: 11, owned: true, productUrl: "https://cdn1.fahasa.com/media/catalog/product/n/x/nxbtre_full_14292025_082912.jpg" },
+    { number: 12, owned: true, productUrl: "https://cdn1.fahasa.com/media/catalog/product/n/x/nxbtre_full_04042025_040425.jpg" }
+  ]
+});
