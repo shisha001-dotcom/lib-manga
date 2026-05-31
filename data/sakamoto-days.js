f0f5b0071279edd09914a2ab44c443eb2a42222d
shisha001-dotcom <shisha001@dulieu16.com>
2026-05-31 13:08:00 +0700

    Create sakamoto-days.js

---
+++ data/sakamoto-days.js
@@ -0,0 +1,28 @@
+books.push({
+  id: "sakamoto-days",
+  title: "Sakamoto Days",
+  author: "Yuto Suzuki",
+  shelf: "next",
+
+  volumes: [
+    { number: 1, owned: true, cover: "https://product.hstatic.net/1000376556/product/nxbtre_full_18572024_035701_1d0fa7d7683b4e4084ba8b7a6b66ce2a_1024x1024.jpg" },
+    { number: 2, owned: true, cover: "https://product.hstatic.net/1000376556/product/sakamoto_days_tap_2_1024x1024.jpg" },
+    { number: 3, owned: true, cover: "https://product.hstatic.net/1000376556/product/sakamoto_days_tap_3_1024x1024.jpg" },
+    { number: 4, owned: true, cover: "https://product.hstatic.net/1000376556/product/sakamoto_days_tap_4_1024x1024.jpg" },
+    { number: 5, owned: true, cover: "https://product.hstatic.net/1000376556/product/sakamoto_days_tap_5_1024x1024.jpg" },
+    { number: 6, owned: true, cover: "https://product.hstatic.net/1000376556/product/sakamoto_days_tap_6_1024x1024.jpg" },
+    { number: 7, owned: true, cover: "https://product.hstatic.net/1000376556/product/sakamoto_days_tap_7_1024x1024.jpg" },
+    { number: 8, owned: true, cover: "https://product.hstatic.net/1000376556/product/sakamoto_days_tap_8_1024x1024.jpg" },
+    { number: 9, owned: true, cover: "https://product.hstatic.net/1000376556/product/sakamoto_days_tap_9_1024x1024.jpg" },
+    { number: 10, owned: true, cover: "https://product.hstatic.net/1000376556/product/sakamoto_days_tap_10_1024x1024.jpg" },
+    { number: 11, owned: true, cover: "https://product.hstatic.net/1000376556/product/sakamoto_days_tap_11_1024x1024.jpg" },
+    { number: 12, owned: true, cover: "https://product.hstatic.net/1000376556/product/sakamoto_days_tap_12_1024x1024.jpg" },
+    { number: 13, owned: true, cover: "https://product.hstatic.net/1000376556/product/sakamoto_days_tap_13_1024x1024.jpg" },
+    { number: 14, owned: true, cover: "https://product.hstatic.net/1000376556/product/sakamoto_days_tap_14_1024x1024.jpg" },
+    { number: 15, owned: true, cover: "https://product.hstatic.net/1000376556/product/sakamoto_days_tap_15_1024x1024.jpg" },
+    { number: 16, owned: true, cover: "https://product.hstatic.net/1000376556/product/sakamoto_days_tap_16_1024x1024.jpg" },
+    { number: 17, owned: true, cover: "https://product.hstatic.net/1000376556/product/sakamoto_days_tap_17_1024x1024.jpg" },
+    { number: 18, owned: true, cover: "https://product.hstatic.net/1000376556/product/sakamoto_days_tap_18_1024x1024.jpg" },
+    { number: 19, owned: true, cover: "https://product.hstatic.net/1000376556/product/sakamoto_days_tap_19_1024x1024.jpg" }
+  ]
+});
